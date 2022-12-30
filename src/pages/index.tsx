1c151c9c1a09791abbd50f8f9ee7b48697932883
TongThanaphon <tongasura001@gmail.com>
2022-12-30 18:12:53 +0700

    init repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Text from "@components/Text";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>Home Page</div>
+      <Text />
+    </div>
+  );
+};
+
+export default HomePage;
